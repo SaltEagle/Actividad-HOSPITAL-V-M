7af523846fbc1ee4584d63e2d7e5939627cb9f0d
salvador.aguilar <sal.aguilar@duocuc.cl>
2025-05-07 16:49:45 -0400

    Agreagado la parte 2

---
+++ src/main/java/com/hospital_vm/cl/hospital/model/Paciente.java
@@ -0,0 +1,35 @@
+package com.hospital_vm.cl.hospital.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name= "paciente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 13, nullable = false)
+    private String run;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = true)
+    private Date fechaNacimiento;
+
+    @Column(nullable = false)
+    private String correo;
+}
