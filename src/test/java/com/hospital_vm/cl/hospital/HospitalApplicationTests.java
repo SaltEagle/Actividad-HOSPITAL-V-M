2351632534f1914db9753e0d16f72aa51ffc1709
salvador.aguilar <sal.aguilar@duocuc.cl>
2025-05-07 15:56:05 -0400

    Agreagado la parte 2

---
+++ src/test/java/com/hospital_vm/cl/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital_vm.cl.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
