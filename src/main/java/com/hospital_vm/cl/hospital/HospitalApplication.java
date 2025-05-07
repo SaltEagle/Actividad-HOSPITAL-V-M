eba78558382ce81ddb72de7a7492470e3af31308
salvador.aguilar <sal.aguilar@duocuc.cl>
2025-05-07 15:56:05 -0400

    Agregado Hospital

---
+++ src/main/java/com/hospital_vm/cl/hospital/HospitalApplication.java
@@ -0,0 +1,13 @@
+package com.hospital_vm.cl.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalApplication.class, args);
+	}
+
+}
